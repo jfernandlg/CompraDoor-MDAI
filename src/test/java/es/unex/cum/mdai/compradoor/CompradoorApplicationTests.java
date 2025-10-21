4a5c0de1ea73bab270906b86e363768b1faaa1e4
jfernandlg <jfernandlg@alumnos.unex.es>
2025-10-21 17:24:00 +0200

    Primer commit Aplicación MDAI CompraDoor

---
+++ src/test/java/es/unex/cum/mdai/compradoor/CompradoorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.cum.mdai.compradoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompradoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
