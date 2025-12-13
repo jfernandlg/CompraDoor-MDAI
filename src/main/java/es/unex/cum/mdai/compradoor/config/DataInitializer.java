5709f79db5091f20c2a4f5d42b456f28e1961143
jfernandlg <jfernandlg@alumnos.unex.es>
2025-12-13 19:52:21 +0100

    Imagenes nuevas de inmuebles

--- src/main/java/es/unex/cum/mdai/compradoor/config/DataInitializer.java
+++ src/main/java/es/unex/cum/mdai/compradoor/config/DataInitializer.java
@@ -90,6 +90,52 @@ public class DataInitializer {
 
                 inmuebleRepository.save(inmueble4);
 
+                Inmueble inmueble5 = new Inmueble();
+                inmueble5.setDireccion("Calle del Conde de Peñalver, 90, 28006 Madrid");
+                inmueble5.setLocalidad("Salamanca");
+                inmueble5.setPrecio(650_000f);
+                inmueble5.setPathFotos(Arrays.asList(
+                        "/images/inmueble5/pexels-curtis-adams-1694007-14495871.jpg",
+                        "/images/inmueble5/pexels-curtis-adams-1694007-14495877.jpg",
+                        "/images/inmueble5/pexels-curtis-adams-1694007-14495890.jpg",
+                        "/images/inmueble5/pexels-curtis-adams-1694007-14495936.jpg",
+                        "/images/inmueble5/pexels-curtis-adams-1694007-14495968.jpg"
+                ));
+
+                inmuebleRepository.save(inmueble5);
+
+                Inmueble inmueble6 = new Inmueble();
+                inmueble6.setDireccion("Calle de la Torrecilla del Leal, 12, 28012 Madrid");
+                inmueble6.setLocalidad("Centro");
+                inmueble6.setPrecio(700_000f);
+                inmueble6.setPathFotos(Arrays.asList(
+                        "/images/inmueble6/pexels-expect-best-79873-323780.jpg",
+                        "/images/inmueble6/francesca-tosolini-4TlrOY2IyUA-unsplash.jpg",
+                        "/images/inmueble6/francesca-tosolini-DmOhItSo49k-unsplash.jpg",
+                        "/images/inmueble6/francesca-tosolini-Gh_UjjYoVwk-unsplash.jpg",
+                        "/images/inmueble6/francesca-tosolini-GyCD3-QbBdw-unsplash (1).jpg",
+                        "/images/inmueble6/francesca-tosolini-lLDh9JppH2c-unsplash.jpg",
+                        "/images/inmueble6/francesca-tosolini-XyGvEj587Mc-unsplash.jpg"
+
+                ));
+
+                inmuebleRepository.save(inmueble6);
+
+                Inmueble inmueble7 = new Inmueble();
+                inmueble7.setDireccion("Carrer de Montcada, 15-23, 08003 Barcelona");
+                inmueble7.setLocalidad("Ciutat Vella");
+                inmueble7.setPrecio(250_000f);
+                inmueble7.setPathFotos(Arrays.asList(
+                        "/images/inmueble7/pexels-binyaminmellish-1396122.jpg",
+                        "/images/inmueble7/francesca-tosolini-87mjpwLYXBw-unsplash.jpg",
+                        "/images/inmueble7/francesca-tosolini-I5uy6tQL4VM-unsplash.jpg",
+                        "/images/inmueble7/francesca-tosolini-kDtuOUIkwrk-unsplash.jpg",
+                        "/images/inmueble7/francesca-tosolini-PKBn5iy1MMA-unsplash.jpg"
+                ));
+
+                inmuebleRepository.save(inmueble7);
+
+
                 System.out.println("---------------------------------------------");
                 System.out.println(" CARGA DE DATOS INICIALES COMPLETADA ");
                 System.out.println("---------------------------------------------");
